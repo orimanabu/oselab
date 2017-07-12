a39e83095cdf14896f1d93a6ff4b97ed58088dde
stack user <stack@localhost>
2017-07-12 12:27:40 +0900

    handle flavor.

--- work/do.sh
+++ work/do.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+if [ x"$#" != x"1" ]; then
+	echo "$0 flavor"
+	exit 1
+fi
+flavor=$1; shift
+
 self=$(hostname -s)
-logdir=../results
+logdir=../results/${flavor}
 bindir=../bin
 
 mkdir -p ${logdir}
 hostname | grep master && ${bindir}/ose_info.sh master > ${logdir}/oc
-hostname | grep master && ${bindir}/etcdctl.sh > ${logdir}/etcd
+hostname | grep master && ${bindir}/etcdctl.sh 2> /dev/null > ${logdir}/etcd
 hostname | grep master && ${bindir}/ose_router.sh > ${logdir}/router
 ${bindir}/ose_iptables.sh > ${logdir}/iptables-${self}
 ${bindir}/ose_network.sh > ${logdir}/network-${self}
