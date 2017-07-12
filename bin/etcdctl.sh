0dd879196b0fdcda07712373567a99eed67a52e0
stack user <stack@localhost>
2017-07-12 12:24:11 +0900

    get member list.

--- bin/etcdctl.sh
+++ bin/etcdctl.sh
@@ -2,8 +2,13 @@
 
 etcdctl="etcdctl --endpoint https://master1.example.com:4001 --cert-file /etc/origin/master/master.etcd-client.crt --key-file /etc/origin/master/master.etcd-client.key --ca-file /etc/origin/master/ca-bundle.crt"
 
+echo "=> cluster-health"
+${etcdctl} cluster-health
+echo "=> member"
+${etcdctl} member list
+echo "=> key/values"
 for key in $(${etcdctl} ls -r -p); do
-	echo "=> ${key}"
+	echo "==> ${key}"
 	echo ${key} | grep '/$' > /dev/null 2>&1
 	if [ x"$?" != x"0" ]; then
 		${etcdctl} get ${key} | python -m json.tool
