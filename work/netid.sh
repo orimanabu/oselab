07c232d2823530f7359e006d101d98c7abd1662c
stack user <stack@localhost>
2017-07-13 16:01:27 +0900

    print hex netid also.

--- work/netid.sh
+++ work/netid.sh
@@ -6,4 +6,10 @@ if [ x"$#" != x"1" ]; then
 fi
 project=$1; shift
 
-etcdctl --endpoint https://master1.example.com:4001 --cert-file /etc/origin/master/master.etcd-client.crt --key-file /etc/origin/master/master.etcd-client.key --ca-file /etc/origin/master/ca-bundle.crt get /openshift.io/registry/sdnnetnamespaces/${project} | python -m json.tool
+output=$(etcdctl --endpoint https://master1.example.com:4001 --cert-file /etc/origin/master/master.etcd-client.crt --key-file /etc/origin/master/master.etcd-client.key --ca-file /etc/origin/master/ca-bundle.crt get /openshift.io/registry/sdnnetnamespaces/${project} 2> /dev/null)
+
+echo ${output}
+
+vnid=$(echo "${output}" | python -m json.tool | grep netid | sed -e 's/.*: \([0-9]\+\),/\1/')
+echo ${vnid}
+printf "0x%x\n" ${vnid}
