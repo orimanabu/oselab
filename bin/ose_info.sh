03ec6ee00ffc8cc86d81b31e8f11f38b3a24c3ed
stack user <stack@localhost>
2017-07-13 16:21:24 +0900

    update for any project.

--- bin/ose_info.sh
+++ bin/ose_info.sh
@@ -8,7 +8,7 @@ function do_command {
 
 function oc_get {
 	local project=$1; shift
-	do_command oc get all -o wide -n ${project}
+	do_command oc get all,pvc -o wide -n ${project}
 	do_command oc get pod -o wide -n ${project}
 	do_command oc get svc -n ${project}
 	do_command oc get route -n ${project}
@@ -19,49 +19,37 @@ if [ x"$#" != x"1" ]; then
 	exit 1
 fi
 op=$1; shift
-project=proj1
+#projects="proj1 proj2"
 
 case ${op} in
 master)
 	echo "=> node"
 	do_command oc get node --show-labels
-
-	echo
-	echo "=> project: default"
-	oc_get default
-
-	echo
-	echo "=> project: ${project}"
-	oc_get ${project}
-
-	echo
 	echo "=> project: all_namespaces"
-	do_command oc get all -o wide --all-namespaces
+	do_command oc get all,pvc -o wide --all-namespaces
 	do_command oc get pod -o wide --all-namespaces
 	do_command oc get svc --all-namespaces
 	do_command oc get route --all-namespaces
 
-	echo
-	echo "==>"
-	for resource in $(oc get pod -o name --all-namespaces); do
-		echo -n '='
-		do_command oc describe ${resource}
-		if [ x"$?" != x"0" ]; then
-			echo -n '='; do_command oc describe ${resource} -n ${project}
-		fi
-	done
-	for resource in $(oc get svc -o name --all-namespaces); do
-		echo -n '='
-		do_command oc describe ${resource}
-		if [ x"$?" != x"0" ]; then
-			echo -n '='; do_command oc describe ${resource} -n ${project}
-		fi
-	done
-	for resource in $(oc get route -o name --all-namespaces); do
-		echo -n '='
-		do_command oc describe ${resource}
-		if [ x"$?" != x"0" ]; then
-			echo -n '='; do_command oc describe ${resource} -n ${project}
+	for project in $(oc get project -o name | sed -e 's,^project/,,'); do
+		if [ x"$?" = x"0" ]; then
+			echo
+			echo "=> project: ${project}"
+			oc_get ${project}
+
+			echo
+
+			echo
+			echo "==>"
+			for resource in $(oc get pod -o name -n ${project}); do
+				echo -n '='; do_command oc describe ${resource} -n ${project}
+			done
+			for resource in $(oc get svc -o name -n ${project}); do
+				echo -n '='; do_command oc describe ${resource} -n ${project}
+			done
+			for resource in $(oc get route -o name -n ${project}); do
+				echo -n '='; do_command oc describe ${resource} -n ${project}
+			done
 		fi
 	done
 	;;
